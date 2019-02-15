8163516ac5799994df24fcc4fb22bcef3e3b2f66
Jesp <jstriker9peru@gmail.com>
2019-02-15 11:44:20 -0500

    Added image slider functionality

--- public/js/components/craigslist.js
+++ public/js/components/craigslist.js
@@ -959,17 +959,51 @@ var Gallery = function (_Component) {
 
     _this.allImgsLoop = function () {
       return _this.state.allImgs.map(function (item, i) {
-        return _react2.default.createElement('div', { key: i, className: 'thumb-img', style: { "backgroundImage": 'url(\'' + item + '\')' } });
+        return _react2.default.createElement('div', { key: i, onClick: _this.clickedThumb.bind(null, i), className: 'thumb-img', style: { "backgroundImage": 'url(\'' + item + '\')' } });
+      });
+    };
+
+    _this.nextBtn = function () {
+      if (_this.state.currentIndex != _this.state.allImgs.length - 1) {
+        _this.setState({
+          currentIndex: _this.state.currentIndex + 1
+        });
+      }
+    };
+
+    _this.prevBtn = function () {
+      if (_this.state.currentIndex != 0) {
+        _this.setState({
+          currentIndex: _this.state.currentIndex - 1
+        });
+      }
+    };
+
+    _this.clickedThumb = function (index) {
+      _this.setState({
+        currentIndex: index
       });
     };
 
     _this.state = {
-      allImgs: ['https://i.ytimg.com/vi/NTvI6Ni-dR8/maxresdefault.jpg', 'https://dsx.weather.com//util/image/w/00138f9a-aa2e-402b-92ff-d001008c624e.jpg?v=at&w=485&h=273&api=7db9fe61-7414-47b5-9871-e17d87b8b6a0', 'https://s.blogcdn.com/slideshows/images/slides/400/662/5/S4006625/slug/l/01-2017-chevrolet-corvette-gs-fd-1-1.jpg', 'https://i.pinimg.com/originals/96/0b/bd/960bbd9cec48d31ac3e75bdb40ba5a0a.jpg', 'http://cdn.whoabella.com/wp-content/uploads/2015/12/coolgirlcars2.jpg', 'https://i.ytimg.com/vi/qvuvjEkeDAw/maxresdefault.jpg']
+      allImgs: '',
+      currentImg: '',
+      currentIndex: 0
     };
     return _this;
   }
 
   _createClass(Gallery, [{
+    key: 'componentWillMount',
+    value: function componentWillMount() {
+      var allImgs = ['https://i.ytimg.com/vi/NTvI6Ni-dR8/maxresdefault.jpg', 'https://dsx.weather.com//util/image/w/00138f9a-aa2e-402b-92ff-d001008c624e.jpg?v=at&w=485&h=273&api=7db9fe61-7414-47b5-9871-e17d87b8b6a0', 'https://s.blogcdn.com/slideshows/images/slides/400/662/5/S4006625/slug/l/01-2017-chevrolet-corvette-gs-fd-1-1.jpg', 'https://i.pinimg.com/originals/96/0b/bd/960bbd9cec48d31ac3e75bdb40ba5a0a.jpg', 'http://cdn.whoabella.com/wp-content/uploads/2015/12/coolgirlcars2.jpg', 'https://i.ytimg.com/vi/qvuvjEkeDAw/maxresdefault.jpg'];
+
+      this.setState({
+        allImgs: allImgs,
+        currentImg: allImgs[this.state.currentIndex]
+      });
+    }
+  }, {
     key: 'render',
     value: function render() {
       var _props = this.props,
@@ -988,15 +1022,15 @@ var Gallery = function (_Component) {
             { className: 'main-image' },
             _react2.default.createElement(
               'div',
-              { className: 'arrows left-arrow' },
-              '<'
+              { className: 'arrows left-arrow', onClick: this.prevBtn },
+              _react2.default.createElement('i', { className: 'fas fa-chevron-left' })
             ),
             _react2.default.createElement(
               'div',
-              { className: 'arrows right-arrow' },
-              '>'
+              { className: 'arrows right-arrow', onClick: this.nextBtn },
+              _react2.default.createElement('i', { className: 'fas fa-chevron-right' })
             ),
-            _react2.default.createElement('div', { className: 'image-1', style: { "backgroundImage": 'url(\'' + this.state.allImgs[0] + '\')' } })
+            _react2.default.createElement('div', { className: 'image-1', style: { "backgroundImage": 'url(\'' + this.state.allImgs[this.state.currentIndex] + '\')' } })
           )
         ),
         _react2.default.createElement(
